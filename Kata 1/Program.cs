5d94e3f16b29881ec4115a4176a7a67e985cf2ca
Jama-student <jamafahad097@gmail.com>
2024-11-12 00:11:02 +0100

    Add files via upload

---
+++ Kata 1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Kata1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome, brave adventurer! What is your name?");
+        string nameinput = Console.ReadLine();
+        Console.WriteLine($"Greetings {nameinput}! Your journey begins now...");
+        Console.WriteLine("On a scale of 1 to 10, how excited are you to start this quest?");
+        int excitedinput = int.Parse(Console.ReadLine());
+
+        if (excitedinput > 7)
+        {
+            Console.WriteLine("Fantastic! You seem ready to take on any challenge!");
+
+        }
+        else
+        {
+            Console.WriteLine("there's always a light! you've got this");
+        }
+    }
+}
