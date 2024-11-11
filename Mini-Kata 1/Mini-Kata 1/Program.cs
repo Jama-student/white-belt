37a01b6a33f4d529a6898b1dc9a7de836ce7dc9a
Jama-student <jamafahad097@gmail.com>
2024-11-12 00:44:42 +0100

    Add files via upload

---
+++ Mini-Kata 1/Mini-Kata 1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MiniKata1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declare and initialize variables
+        string characterName = "Arin";
+        int healthPoints = 100;
+        float attackPower = 15.5f;
+        bool isParalyzed = true;
+        double currencyAmount = 24.75;
+
+        // Display each variable’s value on the console
+        Console.WriteLine($"Character Name: {characterName}");
+        Console.WriteLine($"Health Points: {healthPoints}");
+        Console.WriteLine($"Attack Power: {attackPower}");
+        Console.WriteLine($"Is Paralyzed: {isParalyzed}");
+        Console.WriteLine($"Gold Coins: {currencyAmount}");
+    }
+}
