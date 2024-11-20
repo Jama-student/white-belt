4a19c9cf438ff0dcec4fbbf41d811c11b3964b1f
Jama-student <jamafahad097@gmail.com>
2024-11-20 10:34:34 +0100

    Update Program.cs

--- Mini-Kata 3/Mini-Kata 3/Program.cs
+++ Mini-Kata 3/Mini-Kata 3/Program.cs
@@ -4,16 +4,16 @@ class Program
 {
     static void Main()
     {
-        // Declare a playerLuck variable between 1 and 10
+        
         int playerLuck = 7;
 
-        // Ask the player if they want to attack
+        
         Console.WriteLine("Do you want to attack the enemy? (yes/no)");
         string playerChoice = Console.ReadLine().ToLower();
 
         if (playerChoice == "yes")
         {
-            // Check attack success based on playerLuck
+            
             if (playerLuck > 7)
             {
                 Console.WriteLine("Success! Your attack hits the enemy.");
