7ae063c337ed74893ee36dbe0081ce2c1a8c5f8e
Jama-student <jamafahad097@gmail.com>
2024-11-20 10:32:09 +0100

    Update Program.cs

--- Mini kata 4/Mini kata 4/Program.cs
+++ Mini kata 4/Mini kata 4/Program.cs
@@ -6,14 +6,14 @@ class Program
 {
     static void Main()
     {
-        // Spawning enemies using a for loop
+        
         Console.WriteLine("Spawning enemies using a for loop:");
         for (int i = 1; i <= 5; i++)
         {
             Console.WriteLine($"Enemy #{i} spawned!");
         }
 
-        // Spawning enemies using a while loop
+        
         Console.WriteLine("\nSpawning enemies using a while loop:");
         int enemiesSpawned = 0;
         while (enemiesSpawned < 3)
